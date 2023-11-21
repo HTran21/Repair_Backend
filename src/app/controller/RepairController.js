7867dabcfb044c8aff7c6163e19e2d9650ebb41e
HTran21 <tranhoangtran22225@gmail.com>
2023-11-21 13:11:50 +0700

    duyet don dang ky

--- src/app/controller/RepairController.js
+++ src/app/controller/RepairController.js
@@ -5,30 +5,50 @@ class RepairController {
         try {
             const { ID_User, thietbi, mota, date } = req.body;
             const trangthai = 'N';
+
             pool.getConnection((err, connection) => {
-                connection.query('INSERT INTO repair SET ID_User = ?, ID_item = ?, NgayDK = ?, MoTa = ?, TrangThai =?',
-                    [ID_User, thietbi, date, mota, trangthai], (err, data) => {
-                        if (!err) {
-                            res.json({ message: "Đăng ký sửa chữa thành công" })
-                        }
-                        else {
-                            console.log(err);
-                            res.json({ error: "Đăng ký thất bại" })
-                        }
-                    })
-            })
-        }
-        catch (error) {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({ error: "Lỗi Server Nội Bộ" });
+                }
+
+                // Kiểm tra xem một bản ghi với ID_User và ID_item đã cho đã tồn tại hay chưa
+                connection.query('SELECT * FROM repair WHERE ID_User = ? AND ID_item = ? AND TrangThai = ?', [ID_User, thietbi, trangthai], (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).json({ error: "Lỗi Server Nội Bộ" });
+                    }
+
+                    if (rows.length > 0) {
+                        // Bản ghi đã tồn tại
+                        return res.json({ error: "Đơn đăng ký đã tồn tại" });
+                    } else {
+                        // Bản ghi không tồn tại, tiếp tục với truy vấn INSERT
+                        connection.query('INSERT INTO repair SET ID_User = ?, ID_item = ?, NgayDK = ?, MoTa = ?, TrangThai =?',
+                            [ID_User, thietbi, date, mota, trangthai], (err, data) => {
+                                if (!err) {
+                                    res.json({ message: "Đăng ký sửa chữa thành công" });
+                                } else {
+                                    console.log(err);
+                                    res.json({ error: "Đăng ký thất bại" });
+                                }
+                            });
+                    }
+                });
+            });
+        } catch (error) {
             console.log(error);
+            res.status(500).json({ error: "Lỗi Server Nội Bộ" });
         }
     }
+
 
     listRepair(req, res, next) {
         try {
             const ID_User = req.params.id;
             pool.getConnection((err, connection) => {
                 if (err) throw err;
-                connection.query('SELECT ID_Repair, phong.ID_Phong, day.ID_Day, items.ID_item, MSSV ,nameItem, HoTen, NgayDK, MoTa,TrangThai  FROM repair JOIN users ON repair.ID_User = users.ID_User JOIN phong ON users.ID_Phong = phong.ID_Phong JOIN day ON phong.ID_Day = day.ID_Day JOIN items ON repair.ID_item = items.ID_item WHERE repair.ID_User = ?', [ID_User],
+                connection.query('SELECT ID_Repair, phong.ID_Phong, day.ID_Day, items.ID_item, MSSV ,nameItem, HoTen, NgayDK, NgayDuyet, MoTa, TrangThai  FROM repair JOIN users ON repair.ID_User = users.ID_User JOIN phong ON users.ID_Phong = phong.ID_Phong JOIN day ON phong.ID_Day = day.ID_Day JOIN items ON repair.ID_item = items.ID_item WHERE repair.ID_User = ?', [ID_User],
                     (err, data) => {
                         if (err) return res.json(err);
                         res.json(data);
@@ -47,7 +67,26 @@ class RepairController {
             const TrangThai = 'N';
             pool.getConnection((err, connection) => {
                 if (err) throw err;
-                connection.query('SELECT ID_Repair, phong.ID_Phong, day.ID_Day, items.ID_item, MSSV ,nameItem, HoTen, NgayDK, MoTa,TrangThai  FROM repair JOIN users ON repair.ID_User = users.ID_User JOIN phong ON users.ID_Phong = phong.ID_Phong JOIN day ON phong.ID_Day = day.ID_Day JOIN items ON repair.ID_item = items.ID_item WHERE repair.TrangThai = ?', [TrangThai],
+                connection.query('SELECT ID_Repair, phong.ID_Phong, day.ID_Day, items.ID_item, MSSV ,nameItem, HoTen, NgayDK,NgayDuyet, MoTa,TrangThai  FROM repair JOIN users ON repair.ID_User = users.ID_User JOIN phong ON users.ID_Phong = phong.ID_Phong JOIN day ON phong.ID_Day = day.ID_Day JOIN items ON repair.ID_item = items.ID_item WHERE repair.TrangThai = ?', [TrangThai],
+                    (err, data) => {
+                        if (err) return res.json(err);
+                        res.json(data);
+                    })
+            })
+
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    listAccept(req, res, next) {
+        try {
+            const TrangThai = 'Y';
+            pool.getConnection((err, connection) => {
+                if (err) throw err;
+                connection.query('SELECT ID_Repair, phong.ID_Phong, day.ID_Day, items.ID_item, MSSV ,nameItem, HoTen, NgayDK, NgayDuyet,MoTa,TrangThai  FROM repair JOIN users ON repair.ID_User = users.ID_User JOIN phong ON users.ID_Phong = phong.ID_Phong JOIN day ON phong.ID_Day = day.ID_Day JOIN items ON repair.ID_item = items.ID_item WHERE repair.TrangThai = ?', [TrangThai],
                     (err, data) => {
                         if (err) return res.json(err);
                         res.json(data);
@@ -102,21 +141,70 @@ class RepairController {
         }
     }
 
+
+    accpetRepair(req, res, next) {
+        try {
+            const ID_Repair = req.params.id;
+            const TrangThai = "Y";
+            const { nhanvien, ngayDuyet } = req.body;
+            console.log("ID_staff", ID_Repair);
+
+            console.log("Data", req.body);
+            pool.getConnection((err, connection) => {
+                if (err) throw err;
+                connection.query('UPDATE repair SET ID_Staff = ?, NgayDuyet = ?, TrangThai = ? WHERE ID_Repair = ? ', [nhanvien, ngayDuyet, TrangThai, ID_Repair],
+                    (err, data) => {
+                        if (!err) {
+                            res.json({ message: "Duyệt đơn đăng ký thành công" })
+                        }
+                        else {
+                            console.log(err);
+                            res.json({ error: "Duyệt đơn đăng ký thất bại" })
+                        }
+                    })
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
     deleteRepair(req, res) {
         const id = req.body.id;
         pool.getConnection((err, connection) => {
             if (err) throw err;
             connection.query('DELETE FROM repair WHERE ID_Repair = ?', [id], (err, rows) => {
                 if (!err) {
-                    res.json({ message: "Thiết bị đã được xóa" })
+                    res.json({ message: "Đơn đăng ký đã được xóa" })
                 }
                 else {
                     console.log(err);
-                    res.json({ error: "Xóa thiết bị không thành công" })
+                    res.json({ error: "Xóa đơn đăng ký không thành công" })
                 }
             })
         })
     }
+
+    infoRepairAccept(req, res, next) {
+        try {
+            const ID_Repair = req.params.id;
+            pool.getConnection((err, connection) => {
+                if (err) throw err;
+                connection.query('SELECT ID_Repair, phong.TenPhong, day.TenDay, items.ID_item, MSSV ,nameItem, users.hoten AS hotenUser, staffs.hoten AS hotenNV, users.email, users.phone, NgayDK, MoTa,TrangThai  FROM repair JOIN users ON repair.ID_User = users.ID_User JOIN phong ON users.ID_Phong = phong.ID_Phong JOIN day ON phong.ID_Day = day.ID_Day JOIN items ON repair.ID_item = items.ID_item JOIN staffs ON repair.ID_Staff = staffs.ID_Staff WHERE repair.ID_Repair = ?', [ID_Repair],
+                    (err, data) => {
+                        if (err) return res.json(err);
+                        res.json(data[0]);
+                    })
+            })
+
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
 
 }
 
--- src/routes/repair.js
+++ src/routes/repair.js
@@ -5,9 +5,13 @@ const repairController = require('../app/controller/RepairController');
 
 router.post("/", repairController.registerRepair);
 router.get("/unapproved", repairController.listUnapprovied);
+router.get("/accept", repairController.listAccept);
 router.get("/:id", repairController.listRepair);
+
+router.get("/info/:id", repairController.infoRepairAccept);
 router.get("/update/:id", repairController.infoRepair);
 router.put("/update/:id", repairController.updateRepair);
+router.put("/accept/:id", repairController.accpetRepair);
 router.delete("/delete", repairController.deleteRepair);
 
 module.exports = router;
